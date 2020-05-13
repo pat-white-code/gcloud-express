b578881d70391013c63178aefcb87084f1f07553
pat-white-code <pat.k.white@gmail.com>
2020-05-13 15:22:59 -0500

    comments out welcome to our express app

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to our express app')
-})
+// app.get('/', (req, res) => {
+//     res.send('Welcome to our express app')
+// })
 
 app.get('/test', (req, res)=> {
     res.send('SERVER HIT! BAZINGA!')
