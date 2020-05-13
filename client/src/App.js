9946f73f32154144643e7bfbbcba0fa716efa647
pat-white-code <pat.k.white@gmail.com>
2020-05-13 14:50:33 -0500

    makes express server serve client build

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+function App() { 
+  const handleTest = () => {
+    axios.get('/test').then(res => console.log(res))
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button onClick={handleTest}>
+          test server
+        </button>
+        <a>
           Learn React
         </a>
       </header>
